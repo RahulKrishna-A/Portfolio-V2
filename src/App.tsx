40c02f6c75d9681e7490097bedfdc4a8e78a2301
Rahul Krishna <rahulkrish321123@gmail.com>
2024-09-24 03:07:09 +0530

    Added routes to the app with basic layout Outlet and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./Pages/Layout/Layout.tsx";
 
 function App() {
 
     return (
         <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Layout/>}>
+                        {/*<Route path={"/"} element={<Home/>}></Route>*/}
+                    </Route>
+                </Routes>
+            </BrowserRouter>
 
         </div>
     )
