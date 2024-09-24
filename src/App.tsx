706300b21e4f53c38c095e15899d496b16ec5910
Rahul Krishna <rahulkrish321123@gmail.com>
2024-09-24 11:34:34 +0530

    Added lenis and fortAwesome adn tailwing configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Layout from "./Pages/Layout/Layout.tsx";
-
+import { ReactLenis } from "@studio-freight/react-lenis";
 function App() {
+
+    const lenisOptions = {
+        lerp: 0.1,
+        duration: 1.5,
+        smoothTouch: false, //smooth scroll for touch devices
+        smooth: true,
+    };
+
 
     return (
         <div>
+            <ReactLenis root options={lenisOptions}>
             <BrowserRouter>
                 <Routes>
                     <Route element={<Layout/>}>
@@ -13,7 +22,7 @@ function App() {
                     </Route>
                 </Routes>
             </BrowserRouter>
-
+            </ReactLenis>
         </div>
     )
 }
--- src/Pages/Layout/Layout.tsx
+++ src/Pages/Layout/Layout.tsx
@@ -1,11 +1,37 @@
 import {Outlet} from "react-router-dom";
+import {useState} from "react";
+import Nav from "./Components/Nav.tsx";
 
 export default function Layout(){
 
+    const navItems = [
+        {
+            name: 'home',
+            link: 'home',
+            key: '01.',
+        },
+        { name: 'about', link: 'about', key: '02.' },
+        { name: 'work', link: 'work', key: '03.' },
+        { name: 'contact', link: 'contact', key: '04.' },
+    ]
+    const fonts = {
+        accent: 'text-[#05BFDB] text-sm font-mono',
+        primaryHeading:
+            'whitespace-nowrap text-gray-200 font-bold leading-tight font-sans',
+        secondaryHeading: 'text-gray-400 font-bold leading-tight font-sans',
+        base: 'text-gray-400 text-base font-sans',
+    }
+
+    const [active, setActive] = useState(navItems[0].name)
 
     return(
         <div>
-            <Outlet/>
+            <Nav
+                fonts={fonts}
+                navItems={navItems}
+                active={active}
+                setActive={setActive}
+            />
         </div>
     )
 
